b436d436d8168c56b99b6a4443c242181b5a313c
Iliuta Mihai <mortvmis@gmail.com>
2019-05-15 02:11:58 +0300

    cleanup

--- main.py
+++ main.py
@@ -1,12 +1,43 @@
+import sys
+from os import listdir
+from os.path import isdir, isfile, join
 from flask import Flask, render_template, request, send_from_directory
 
 # FLASK SETUP
 app = Flask(__name__, static_url_path='')
 
+def models(path):
+    all_models = [f for f in listdir(path) if isdir(join(path, f))]
+    return all_models
 
-@app.route('/')
+
+def maps(path):
+    all_maps = [f for f in listdir(path) if isfile(join(path, f))]
+    return all_maps
+
+
+@app.route('/', methods=['GET', 'POST'])
 def home():
-    return render_template('test.html')
+
+    models_path = 'js/models/gltf/'
+    maps_path = 'js/textures/equirectangular'
+    all_models = models(models_path)
+    all_maps =  maps(maps_path)
+
+
+
+    if request.method == 'POST':
+        selected_model = request.form.get('model_select')
+        model_dir_path = models_path + '/' + selected_model + '/glTF/'
+        model_path = selected_model + '.gltf'
+    else:
+        model_dir_path = models_path + '/' + 'Duck' + '/glTF/'
+        model_path = 'Duck' + '.gltf'
+
+    return render_template('test.html', model_dir_path = model_dir_path,
+                                        model_file_path = model_path,
+                                        all_models = all_models,
+                                        all_maps = all_maps)
 
 
 @app.route('/js/<path:path>')
