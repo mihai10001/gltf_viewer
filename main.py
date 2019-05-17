6de01d84023167e18e1a0cfad6c3e48725bc526e
Iliuta Mihai <mortvmis@gmail.com>
2019-05-17 03:17:36 +0300

    finished dynamic logic

--- main.py
+++ main.py
@@ -5,7 +5,8 @@ from flask import Flask, render_template, request, send_from_directory
 
 # FLASK SETUP
 app = Flask(__name__, static_url_path='')
-
+default_selected_model = 'Duck'
+default_selected_map = 'venice_sunset_2k.hdr'
 
 def models(path):
     all_models = [f for f in listdir(path) if isdir(join(path, f))]
@@ -24,20 +25,26 @@ def home():
     maps_path = 'js/textures/equirectangular'
     all_models = models(models_path)
     all_maps = maps(maps_path)
+    global default_selected_model, default_selected_map
 
     if request.method == 'POST':
-        selected_model = request.form.get('model_select')
-        model_dir_path = models_path + '/' + selected_model + '/glTF/'
-        model_path = selected_model + '.gltf'
-    else:
-        model_dir_path = models_path + '/' + 'Duck' + '/glTF/'
-        model_path = 'Duck' + '.gltf'
+        default_selected_model = request.form.get('model_select', default_selected_model)
+        default_selected_map = request.form.get('map_select', default_selected_map)
 
-    return render_template('test.html',
+    model_dir_path = models_path + '/' + default_selected_model + '/glTF/'
+    model_path = default_selected_model + '.gltf'
+    map_dir_path = maps_path + '/' 
+    map_path = default_selected_map
+
+    return render_template('home.html',
                            model_dir_path=model_dir_path,
                            model_file_path=model_path,
+                           map_dir_path=map_dir_path,
+                           map_file_path=map_path,
                            all_models=all_models,
-                           all_maps=all_maps)
+                           all_maps=all_maps,
+                           sel_model=default_selected_model,
+                           sel_map=default_selected_map)
 
 
 @app.route('/js/<path:path>')
